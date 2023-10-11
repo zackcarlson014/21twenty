04de9502594a5d783d474ed20ae153c83fb43a9d
zackcarlson014 <jzcarlson014@gmail.com>
2023-10-11 05:57:03 -0500

    auth flow updated

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,31 @@
 <template>
-  <authenticator>
-    <template v-slot="{ user, signOut }">
-      <h1>Hello {{ user.username }}!</h1>
-      <button @click="signOut">Sign Out</button>
-    </template>
-  </authenticator>
+  <div class="app">
+    <v-container fluid>
+      <authenticator>
+        <template v-slot="{ user, signOut }">
+          <nav>
+            <RouterLink to='/'>Home</RouterLink> | 
+            <RouterLink to='/about'>About</RouterLink>
+          </nav>
 
-  <div id="app">
-    <h1>Todo App</h1>
+          <RouterView />
 
-    <input type="text" v-model="name" placeholder="Todo name" />
+          <h1>Hello {{ user.username }}!</h1>
 
-    <input type="text" v-model="description" placeholder="Todo description" />
-
-    <button v-on:click="requestCreateTodo">Create Todo</button>
-
-    <div v-for="item in todos" :key="item.id">
-      <h3>{{ item.name }}</h3>
-      <p>{{ item.description }}</p>
-    </div>
-
+          <button @click="signOut">Sign Out</button>
+        </template>
+      </authenticator>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { API } from 'aws-amplify';
-  import { GraphQLResult, GraphQLSubscription } from "@aws-amplify/api";
   import { Authenticator } from '@aws-amplify/ui-vue';
   import '@aws-amplify/ui-vue/styles.css';
-  import { onMounted } from 'vue'
-  import { createTodo } from './graphql/mutations';
-  import { listTodos } from './graphql/queries';
-  import { onCreateTodo } from './graphql/subscriptions';
-  import { Todo, useTodosStore } from './stores/todos';
-
-
-  interface EventData {
-    value: {
-      data: {
-        onCreateTodo: any;
-      };
-    };
-  }
-
-  const todosStore = useTodosStore();
-
-  const requestCreateTodo = async () => {
-    if (!todosStore.name || !todosStore.description)
-      return;
-
-    const todo: Todo = {
-      name: todosStore.name,
-      description: todosStore.description
-    };
-
-    todosStore.addTodo(todo);
-
-    await API.graphql({
-      query: createTodo,
-      variables: { input: todo }
-    });
-
-    todosStore.name = '';
-    todosStore.description = '';
-  }
-
-  const getTodos = async () => {
-    console.log({ this: this });
-    const todosResult: GraphQLResult<any> = await API.graphql({
-      query: listTodos
-    });
-
-    todosStore.setTodos(
-      todosResult.data.listTodos.items
-    );
-  }
-      
-  const subscribe = async () => {
-    (API.graphql({ query: onCreateTodo }) as GraphQLSubscription<any>)
-      .subscribe({
-        next: (eventData: EventData) => {
-          const todo = eventData.value.data.onCreateTodo;
-
-          if (todosStore.todos.some((item) => item.name === todo.name))
-            return; // remove duplications
-
-          todosStore.addTodo(todo);
-        }
-      });
-  }
-
-  onMounted(async () => {
-    await getTodos();
-    await subscribe();
-  })
 </script>
+
+<style>
+.app {
+  text-align: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,88 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>Todo App</h1>
+
+    <input type="text" v-model="name" placeholder="Todo name" />
+
+    <input type="text" v-model="description" placeholder="Todo description" />
+
+    <button v-on:click="requestCreateTodo">Create Todo</button>
+
+    <div v-for="item in todos" :key="item.id">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
+    </div>
+
+  </div>
 </template>
+
+<script lang="ts" setup>
+  import { API, Auth } from 'aws-amplify';
+  import { GraphQLResult, GraphQLSubscription } from "@aws-amplify/api";
+  import { onMounted } from 'vue'
+  import { createTodo } from '../graphql/mutations';
+  import { listTodos } from '../graphql/queries';
+  import { onCreateTodo } from '../graphql/subscriptions';
+  import { Todo, useTodosStore } from '../stores/todos';
+
+
+  interface EventData {
+    value: {
+      data: {
+        onCreateTodo: any;
+      };
+    };
+  }
+
+  const todosStore = useTodosStore();
+
+  const requestCreateTodo = async () => {
+    if (!todosStore.name || !todosStore.description)
+      return;
+
+    const todo: Todo = {
+      name: todosStore.name,
+      description: todosStore.description
+    };
+
+    todosStore.addTodo(todo);
+
+    await API.graphql({
+      query: createTodo,
+      variables: { input: todo }
+    });
+
+    todosStore.name = '';
+    todosStore.description = '';
+  }
+
+  const getTodos = async () => {
+    console.log({ Auth });
+    const todosResult: GraphQLResult<any> = await API.graphql({
+      query: listTodos
+    });
+
+    todosStore.setTodos(
+      todosResult.data.listTodos.items
+    );
+  }
+      
+  const subscribe = async () => {
+    (API.graphql({ query: onCreateTodo }) as GraphQLSubscription<any>)
+      .subscribe({
+        next: (eventData: EventData) => {
+          const todo = eventData.value.data.onCreateTodo;
+
+          if (todosStore.todos.some((item) => item.name === todo.name))
+            return; // remove duplications
+
+          todosStore.addTodo(todo);
+        }
+      });
+  }
+
+  onMounted(async () => {
+    await getTodos();
+    await subscribe();
+  })
+</script>
